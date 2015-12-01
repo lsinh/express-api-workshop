dc73d185953077223b57399f4c27151d09e98373
lsinh <leichin.sinha@gmail.com>
2015-12-01 19:33:06 +0000

    exercise-3

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
-var db = require('mysql');
-
-var connection = db.createConnection({
-    user: 'ziad_saab',
-    host: '127.0.0.1',
-    database: 'addressbook'
-});
-
 var app = express();
 app.use(bodyParser.json());
 
-// app.get('...
+var db = require('mysql');
 
-app.use(function(request, response, next) {
-    request.accountId === 1;
+
+var connection = db.createConnection({
+    user: 'lsinh',
+    host: 'localhost',
+    database: 'addressbook'
+});
+
+connection.connect();
+
+
+
+app.get('/hello', function (req, res) {
+  res.send('<h1>Hello World! </h1>');
+});
+
+
+//Exercise 1
+app.use(function(req, res, next) {
+    req.accountId = 1;
+    console.log(req.accountId);
     next();
-})
+});
+
+
+//Exercise 2
+app.get('/AddressBook', function(req, res) {
+    
+    connection.query('select * from AddressBook where AddressBook.accountId =' + req.accountId, function(err, rows) {
+        if (err) {
+            console.log("Error Found");
+        }
+        else {
+            res.json(rows);
+        };
+    });
+});
+
+//Exercise 3
+
+
+app.listen(process.env.PORT, function(){
+    console.log("SKYNET IS ONLINE!")
+});
+
+//Exercise 3
+
+
